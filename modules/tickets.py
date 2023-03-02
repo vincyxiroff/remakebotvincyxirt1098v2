3a8a639b0f01e65106c245fef150d878a62b7eda
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-03-02 23:13:20 +0100

    Update tickets.py

--- modules/tickets.py
+++ modules/tickets.py
@@ -10,3 +10,12 @@ from utils import remove_command_message, check_permissions
 
 db = Database(False)
 config = yaml.safe_load(open("modules/ticketsConfig.yml", 'r', encoding="utf-8"))
+
+class Tickets(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        db.check_create_table("tickets", "id INT AUTO_INCREMENT PRIMARY KEY, ticket_channel_id VARCHAR(64),"
+                                         " ticket_category VARCHAR(255), creator_id VARCHAR(64),"
+                                         " is_closed BOOLEAN, creation_date DATE")
+        print('Tickets workanti!')
+
