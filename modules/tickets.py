2a96d8dee6ac89ec5d31bc2569e284249cda0701
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-03-02 23:25:03 +0100

    Update tickets.py

--- modules/tickets.py
+++ modules/tickets.py
@@ -166,3 +166,84 @@ class Tickets(commands.Cog):
             if not db.get_value_general("tickets", "is_closed", f"ticket_channel_id={channel.id}"):
                 await self.close_ticket(channel, True)
                 print("Ticket chiusura forzata, il canale è stato eliminato!")
+
+
+    @commands.hybrid_command(description="Chiude il ticket")
+    async def close(self, ctx):
+        channel = ctx.message.channel
+        if db.check_contains("tickets", "ticket_channel_id", str(channel.id)):
+            if not db.get_value_general("tickets", "is_closed", f"ticket_channel_id={channel.id}"):
+                await self.close_ticket(channel, False)
+
+
+
+    @staticmethod
+    async def confirm_close_ticket(interaction):
+        embed_conf = config.get("confirm-close-embed")
+        embed = discord.Embed(title=embed_conf.get("embed-title"),
+                              description=embed_conf.get("embed-description"),
+                              color=embed_conf.get("embed-colour"))
+        embed.set_footer(text=embed_conf.get("embed-footer"))
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label=embed_conf.get("confirm-button"), custom_id="tickets_confirm_close_ticket", style=ButtonStyle.gray))
+        await interaction.response.send_message(embed=embed,
+                                  ephemeral=True,
+                                  view=view)
+
+    async def close_ticket(self, channel, force_closed):
+        ticket_id = db.get_id("tickets", "ticket_channel_id=" + str(channel.id))
+        db.update_data("tickets", "is_closed", "1", f"id={str(ticket_id)}")
+        if not force_closed:
+            if config.get("send-transcription"):
+                await self.create_transcript(ticket_id, channel)
+            await channel.delete()
+        print(f"Ticket {str(ticket_id)} chiuso.")
+
+    async def create_transcript(self, ticket_id, channel):
+        try:
+            transcript = await chat_exporter.export(channel)
+            if transcript is None:
+                return
+        except Exception as e:
+            print("Si è verificato un errore durante il tentativo di creare la trascrizione del ticket:")
+            print(e)
+            return
+        ticket_creator = await self.bot.fetch_user(db.get_value_general("tickets", "creator_id", f"id={ticket_id}"))
+        if ticket_creator:
+            try:
+                await ticket_creator.send(content=str(config.get("transcript-description")),
+                                          file=discord.File(io.BytesIO(transcript.encode()),
+                                                            filename=f"{channel.name}.html", ))
+            except Exception:
+                print(f"Impossibile inviare la trascrizione del ticket all'autore del ticket. ({channel.name})")
+        ticket_log_channel = get(self.bot.get_all_channels(), id=config.get("transcript-channel"))
+        if ticket_log_channel:
+            await ticket_log_channel.send(
+                file=discord.File(io.BytesIO(transcript.encode()), filename=f"{channel.name}.html"))
+
+    async def create_generator(self, ctx):
+        econfig = config.get("ticket-creator-embed")
+        embed = discord.Embed(title=econfig.get("embed-title"),
+                              description=econfig.get("embed-description"),
+                              colour=econfig.get("embed-colour"))
+        embed.set_footer(text=econfig.get("embed-footer"))
+
+        view = discord.ui.View()
+        for category in config.get("categories"):
+            button_conf = config.get("categories").get(category)
+            b_emoji = button_conf.get("emoji")
+            if b_emoji == '':
+                view.add_item(discord.ui.Button(label=button_conf.get("name"), custom_id="tickets_" + category, style=ButtonStyle.gray))
+            else:
+                view.add_item(discord.ui.Button(label=button_conf.get("name"), emoji=b_emoji, custom_id="tickets_" + category, style=ButtonStyle.gray))
+        await ctx.reply(embed=embed, view=view)
+        print("Ticket generator created!")
+
+    @staticmethod
+    async def send_error_message(channel, error_message):
+        embed = discord.Embed(title="Bot ERROR", description=error_message, colour=0xff0000)
+        await channel.send(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(Tickets(bot))
